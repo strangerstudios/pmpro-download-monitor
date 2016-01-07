0de728abf775c5a0f131ca22ce4b8cd312f31a96
Jason Coleman <jason@strangerstudios.com>
2016-01-06 20:30:14 -0500

    first commit

---
+++ pmpro-download-monitor.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Plugin Name: Paid Memberships Pro - Download Monitor Integration Add On
+Plugin URI: http://www.paidmembershipspro.com/pmpro-download-monitor/
+Description: Require membership for downloads when using the Download Monitor plugin.
+Version: .1
+Author: Stranger Studios
+Author URI: http://www.strangerstudios.com
+
+/*
+ * Add Require Membership box to dlm_download CPT
+ */
+function pmprodlm_page_meta_wrapper() {
+	add_meta_box( 'pmpro_page_meta', 'Require Membership', 'pmpro_page_meta', 'dlm_download', 'side' );
+}
+function pmprodlm_cpt_init() {
+	if ( is_admin() )
+		add_action( 'admin_menu', 'pmprodlm_page_meta_wrapper' );
+}
+add_action( "init", "pmprodlm_cpt_init", 20 );
+
+/*
+ * Require Membership on the Download
+*/
+function pmprodlm_can_download( $download, $version ) {
+	if ( function_exists( 'pmpro_hasMembershipLevel' ) ) {
+		if ( ! pmpro_has_membership_access( $version->post->ID ) ) {
+			$download = false;
+		}
+	}
+	return $download;
+}
+add_filter( 'dlm_can_download', 'pmprodlm_can_download', 10, 2 );
+
+function pmprodlm_shortcode_download_content( $content, $download_id, $atts ) {
+	global $current_user;
+	if ( function_exists( 'pmpro_hasMembershipLevel' ) ) {
+		if ( !pmpro_has_membership_access( $download_id ) ) 
+		{
+			$hasaccess = pmpro_has_membership_access($download_id, NULL, true);
+			if(is_array($hasaccess))
+			{
+				//returned an array to give us the membership level values
+				$post_membership_levels_ids = $hasaccess[1];
+				$post_membership_levels_names = $hasaccess[2];
+				$hasaccess = $hasaccess[0];
+			}
+			if(empty($post_membership_levels_ids))
+				$post_membership_levels_ids = array();
+			if(empty($post_membership_levels_names))
+				$post_membership_levels_names = array();
+		
+			 //hide levels which don't allow signups by default
+			if(!apply_filters("pmpro_membership_content_filter_disallowed_levels", false, $post_membership_levels_ids, $post_membership_levels_names))
+			{
+				foreach($post_membership_levels_ids as $key=>$id)
+				{
+					//does this level allow registrations?
+					$level_obj = pmpro_getLevel($id);
+					if(!$level_obj->allow_signups)
+					{
+						unset($post_membership_levels_ids[$key]);
+						unset($post_membership_levels_names[$key]);
+					}
+				}
+			}
+		
+			$post_membership_levels_names = pmpro_implodeToEnglish($post_membership_levels_names, 'or');
+			
+			$download = new DLM_Download( $download_id );
+			if ( $download->exists() ) {
+				$content .= '<a href="' . pmpro_url('levels') . '">' . $download->get_the_title() . '</a>';
+				$content .= ' (' . __('Membership Required','pmprodlm') . ': ' . $post_membership_levels_names . ')';
+			} else {
+				$content = '[' . __( 'Download not found', 'download-monitor' ) . ']';
+			}
+		}
+	}
+	return $content;
+}
+add_filter('dlm_shortcode_download_content', 'pmprodlm_shortcode_download_content', 10, 3);
+
+/*
+Function to add links to the plugin row meta
+*/
+function pmprodlm_plugin_row_meta($links, $file) {
+	if(strpos($file, 'pmpro-download-monitor.php') !== false)
+	{
+		$new_links = array(
+			'<a href="' . esc_url('http://www.paidmembershipspro.com/add-ons/plus-add-ons/pmpro-download-monitor/')  . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
+			'<a href="' . esc_url('http://paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
+		);
+		$links = array_merge($links, $new_links);
+	}
+	return $links;
+}
+add_filter('plugin_row_meta', 'pmprodlm_plugin_row_meta', 10, 2);
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Paid Memberships Pro - Download Monitor Integration Add On ===
+Contributors: strangerstudios
+Tags: paid memberships pro, pmpro, membership, memberships, download monitor, restrict downloads
+Requires at least: 3.5
+Tested up to: 4.4
+Stable tag: .1
+
+Require membership for downloads when using the Download Monitor plugin.
+
+== Description ==
+
+The Download Monitor Integration Add On for Paid Memberships Pro adds a "Require Membership" meta box to the "Edit Download" page, allowing you to easily toggle the membership level(s) that can access the download. 
+
+When using the [download] shortcode, the non-member will see a link to the membership levels page and a list of the levels that are required to download the file.
+
+Requires Download Monitor (https://wordpress.org/plugins/download-monitor/) and Paid Memberships Pro installed and activated.
+
+== Installation ==
+
+= Prerequisites =
+1. You must have Paid Memberships Pro and Download Monitor installed and activated on your site.
+
+= Download, Install and Activate! =
+1. Download the latest version of the plugin.
+1. Unzip the downloaded file to your computer.
+1. Upload the /pmpro-download-monitor/ directory to the /wp-content/plugins/ directory of your site.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+= How to Use =
+
+1. After activation, navigate to "Downloads" to Edit or Add a New Download.
+1. Check the box for each level that can access this download in the "Require Membership" meta box (below the Publish box in the right sidebar). 
+1. Save your changes by clicking the "Update" button (or "Publish" if you are creating a new download).
+
+== Changelog ==
+
+= .1 =
+* Initial release.
